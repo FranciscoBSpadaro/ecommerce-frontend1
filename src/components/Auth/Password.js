b375b2173850ce2a2b4fd3940c1b121692d67a89
FranciscoBS89 <francisco.spadaro@outlook.com>
2023-11-27 18:35:08 -0300

    ajustes em botoes

--- src/components/Auth/Password.js
+++ src/components/Auth/Password.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import api from '../../api';
 import { toast, ToastContainer } from 'react-toastify';
 import Lottie from 'lottie-react';
@@ -8,13 +9,19 @@ const Password = () => {
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [showNewPasswordField, setShowNewPasswordField] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   const verifyCurrentPassword = async () => {
     try {
-      const response = await api.post('/password/verify', { password: currentPassword });
+      const response = await api.post('/password/verify', {
+        password: currentPassword,
+      });
       if (response.status === 200) {
-        toast.success(response.data.message)
-        setShowNewPasswordField(true);
+        if (!toast.isActive('successToast')) {
+          toast.success(response.data.message, { toastId: 'successToast' });
+          setShowNewPasswordField(true);
+          setIsButtonDisabled(true);
+        }
       }
     } catch (error) {
       if (error.response) {
@@ -24,23 +31,31 @@ const Password = () => {
       } else {
         toast.error('Erro interno.');
       }
+      setIsButtonDisabled(false);
     }
   };
 
+  const navigate = useNavigate();
+
   const handlePasswordChange = async () => {
     try {
-      if (newPassword.length < 8 || !/[A-Z]/.test(newPassword) || !/[a-z]/.test(newPassword) || !/[0-9]/.test(newPassword)) {
-        toast.error('A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas e números.');
+      if (
+        newPassword.length < 8 ||
+        !/[A-Z]/.test(newPassword) ||
+        !/[a-z]/.test(newPassword) ||
+        !/[0-9]/.test(newPassword)
+      ) {
+        toast.error(
+          'A senha deve ter no mínimo 8 caracteres, incluindo letras maiúsculas, minúsculas e números.',
+        );
         return;
       }
-      await api.put('/password',
-        { password: newPassword },
-      );
+      await api.put('/password', { password: newPassword });
       toast.success('Senha alterada com sucesso.');
-      // Limpar todos os campos do formulário
-      setCurrentPassword('');
-      setNewPassword('');
-      setShowNewPasswordField(false);
+      // Redirecionar para a página inicial após 5 segundos
+      setTimeout(() => {
+        navigate('/');
+      }, 5000);
     } catch (error) {
       if (error.response) {
         toast.error(error.response.data.message);
@@ -63,19 +78,27 @@ const Password = () => {
       <input
         type="password"
         value={currentPassword}
-        onChange={(e) => setCurrentPassword(e.target.value)}
+        onChange={e => setCurrentPassword(e.target.value)}
         placeholder="Senha Atual"
       />
-      <button onClick={verifyCurrentPassword}>Verificar Senha Atual</button>
+      <button
+        className="button-login-a"
+        onClick={verifyCurrentPassword}
+        disabled={isButtonDisabled}
+      >
+        Verificar Senha Atual
+      </button>
       {showNewPasswordField && (
         <>
           <input
             type="password"
             value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
+            onChange={e => setNewPassword(e.target.value)}
             placeholder="Nova senha"
           />
-          <button onClick={handlePasswordChange}>Salvar</button>
+          <button className="button-login-a" onClick={handlePasswordChange}>
+            Salvar
+          </button>
         </>
       )}
     </div>
